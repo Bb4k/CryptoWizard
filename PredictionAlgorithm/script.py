7ec6a7d47453a48b9c42017ea3b6b494a01618f2
DragosBalmau <balmaudragos@gmail.com>
2022-05-14 05:18:43 +0300

    Add prediction for 6 crypto

--- PredictionAlgorithm/script.py
+++ PredictionAlgorithm/script.py
@@ -1,3 +1,5 @@
+import requests
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -7,63 +9,125 @@ from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 from tensorflow.keras.models import Sequential
 
-crypto_currency = "BTC"
+# BTC, Ethereum, Elrond, Solana, Dogecoin, Binance Coin
+crypto_currencies = [
+    {
+        'name': "BTC",
+        'year': 2016,
+        'month': 1,
+        'day': 1
+    },
+    {
+        'name': "ETH",
+        'year': 2017,
+        'month': 1,
+        'day': 1
+    },
+    {
+        'name': "EGLD",
+        'year': 2020,
+        'month': 10,
+        'day': 10
+    },
+    {
+        'name': "SOL",
+        'year': 2020,
+        'month': 10,
+        'day': 10
+    },
+    {
+        'name': "DOGE",
+        'year': 2017,
+        'month': 1,
+        'day': 1
+    },
+    {
+        'name': "BNB",
+        'year': 2018,
+        'month': 1,
+        'day': 1
+    }
+]
 against_currency = "USD"
-start = dt.datetime(2016, 1, 1)
-end = dt.datetime.now()
-data = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo', start, end)
 
-# Prepare Data
-scaler = MinMaxScaler(feature_range=(0, 1))
-scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
+for crypto_currency in crypto_currencies:
+    start = dt.datetime(crypto_currency['year'], crypto_currency['month'], crypto_currency['day'])
+    end = dt.datetime.now()
 
-prediction_days = 100
-x_train, y_train = [], []
+    data = web.DataReader(f'{crypto_currency["name"]}-{against_currency}', 'yahoo', start, end)
 
-for x in range(prediction_days, len(scaled_data)):
-    x_train.append(scaled_data[x - prediction_days:x, 0])
-    y_train.append(scaled_data[x, 0])
+    # Prepare Data
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
 
-x_train, y_train = np.array(x_train), np.array(y_train)
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+    prediction_days = 100
+    x_train, y_train = [], []
 
-model = Sequential()
-model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1))), model.add(Dropout(0.2))
-model.add(LSTM(units=50, return_sequences=True)),
-model.add(Dropout(0.2))
-model.add(LSTM(units=50))
-model.add(Dropout(0.2))
-model.add(Dense(units=1))
+    for x in range(prediction_days, len(scaled_data)):
+        x_train.append(scaled_data[x - prediction_days:x, 0])
+        y_train.append(scaled_data[x, 0])
 
-model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(x_train, y_train, epochs=25, batch_size=32)
+    x_train, y_train = np.array(x_train), np.array(y_train)
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
-test_start = dt.datetime(2020, 1, 1) + dt.timedelta(days=-prediction_days)
-test_end = dt.datetime.now()
+    model = Sequential()
+    model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1))), model.add(Dropout(0.2))
+    model.add(LSTM(units=50, return_sequences=True)),
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=1))
 
-test_data = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo', test_start, test_end)
-actual_prices = test_data['Close'].values
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    model.fit(x_train, y_train, epochs=25, batch_size=32)
 
-total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+    test_start = dt.datetime(2020, 10, 10) + dt.timedelta(days=-prediction_days)
+    test_end = dt.datetime.now()
 
-model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
-model_inputs = model_inputs.reshape(-1, 1)
-model_inputs = scaler.fit_transform(model_inputs)
+    test_data = web.DataReader(f'{crypto_currency["name"]}-{against_currency}', 'yahoo', test_start, test_end)
+    actual_prices = test_data['Close'].values
 
-x_test = []
+    total_dataset = pd.concat((data["Close"], test_data["Close"]), axis=0)
 
-for x in range(prediction_days, len(model_inputs)):
-    x_test.append(model_inputs[x - prediction_days:x, 0])
+    model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
 
-x_test = np.array(x_test)
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-prediction_prices = model.predict(x_test)
-prediction_prices = scaler.inverse_transform(prediction_prices)
+    support_array = total_dataset[
+                    len(total_dataset) - len(test_data) - prediction_days - len(test_data):len(total_dataset) - len(
+                        test_data) - len(test_data)]
+    support_array = np.array(support_array)
 
-plt.figure(figsize=(10, 10))
-plt.plot(actual_prices, color='black', label='Actual Prices')
-plt.plot(prediction_prices, color='green', label='Predicted Prices')
-plt.title(f'{crypto_currency} price prediction'), plt.xlabel('Time')
-plt.ylabel('Price')
-plt.legend(loc='upper left')
-plt.show()
+    model_inputs2 = model_inputs[prediction_days:]
+    support_array2 = np.concatenate([support_array, model_inputs2])
+
+    model_inputs = support_array2
+    model_inputs = model_inputs.reshape(-1, 1)
+    model_inputs = scaler.fit_transform(model_inputs)
+
+    x_test = []
+
+    for x in range(prediction_days, len(model_inputs)):
+        x_test.append(model_inputs[x - prediction_days:x, 0])
+
+    x_test = np.array(x_test)
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+    prediction_prices = model.predict(x_test)
+    prediction_prices = scaler.inverse_transform(prediction_prices)
+
+    plt.figure(figsize=(20, 10))
+    plt.plot(actual_prices, color='black', label='Actual Prices')
+    plt.plot(prediction_prices, color='green', label='Predicted Prices')
+    plt.title(f'{crypto_currency["name"]} price prediction'), plt.xlabel('Time')
+    plt.ylabel('Price')
+    plt.legend(loc='upper left')
+    plt.show()
+
+    print(actual_prices)
+    print(prediction_prices)
+    print(len(prediction_prices), len(actual_prices))
+
+    real_data = [model_inputs[len(model_inputs) - prediction_days:len(model_inputs) + 1, 0]]
+    real_data = np.array(real_data)
+    real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
+    prediction = model.predict(real_data)
+    prediction = scaler.inverse_transform(prediction)
+    print(prediction)
